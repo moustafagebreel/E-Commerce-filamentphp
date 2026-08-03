880c1fd7c58930c543e5d1602023f85aa5ceca43
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:34:28 +0300

    i18n(ar): add Arabic checkout and order summary translations

---
+++ resources/lang/ar/checkout.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'checkout_title' => 'إتمام الطلب',
+    'shipping_address' => 'عنوان الشحن',
+    'payment_method' => 'طريقة الدفع',
+    'order_summary' => 'ملخص الطلب',
+    'subtotal' => 'المجموع الفرعي',
+    'shipping_fee' => 'رسوم الشحن',
+    'coupon_discount' => 'خصم القسيمة',
+    'grand_total' => 'المجموع الكلي',
+    'apply_coupon' => 'تطبيق القسيمة',
+    'place_order' => 'تأكيد الطلب الآن',
+    'cash_on_delivery' => 'الدفع عند الاستلام',
+    'stripe' => 'بطاقة ائتمان / Stripe',
+];
