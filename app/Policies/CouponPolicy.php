bfc498ad8d9d29b172a8bae11f1cefe88b4eb90b
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:57:35 +0300

    feat(policies): implement CouponPolicy authorization rules

---
+++ app/Policies/CouponPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\Coupon;
+use App\Models\User;
+
+class CouponPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, Coupon $Coupon): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, Coupon $Coupon): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, Coupon $Coupon): bool
+    {
+        return true;
+    }
+}
