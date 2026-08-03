2dbb139d25af9409e532c286471130a5a204b14c
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:38:32 +0300

    feat(requests): implement StoreAttributeFormRequest validation class

---
+++ app/Http/Requests/StoreAttributeFormRequest.php
@@ -0,0 +1,20 @@
+﻿<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreAttributeFormRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            // Custom rules for StoreAttributeFormRequest
+        ];
+    }
+}
