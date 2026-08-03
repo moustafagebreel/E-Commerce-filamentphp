3860a3b8a99225e22f12691fd2fc74ccc230ee12
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:34:17 +0300

    i18n(en): add checkout and order summary translations

---
+++ resources/lang/en/checkout.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'checkout_title' => 'Checkout Overview',
+    'shipping_address' => 'Shipping Address',
+    'payment_method' => 'Payment Method',
+    'order_summary' => 'Order Summary',
+    'subtotal' => 'Subtotal',
+    'shipping_fee' => 'Shipping Fee',
+    'coupon_discount' => 'Coupon Discount',
+    'grand_total' => 'Grand Total',
+    'apply_coupon' => 'Apply Coupon',
+    'place_order' => 'Place Order',
+    'cash_on_delivery' => 'Cash On Delivery',
+    'stripe' => 'Stripe / Credit Card',
+];
