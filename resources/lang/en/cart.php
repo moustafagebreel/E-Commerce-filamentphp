7c1bd041adcca7e8cedc4e849facc1f6e0c4a698
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:45:33 +0300

    i18n(cart): add localized string key definitions set 12

--- resources/lang/en/cart.php
+++ resources/lang/en/cart.php
@@ -1,7 +1,7 @@
 ﻿<?php
 
 return [
-    'key_11' => 'Value English 11 for cart',
-    'title_11' => 'Title English 11 for cart',
-    'description_11' => 'Description English 11 for cart',
+    'key_12' => 'Value English 12 for cart',
+    'title_12' => 'Title English 12 for cart',
+    'description_12' => 'Description English 12 for cart',
 ];
