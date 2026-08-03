2dc2960d236c7509fa4295348ad3a083f5b5a5c8
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:38:47 +0300

    feat(policies): add ContactMessageAccessPolicy authorization rules

---
+++ app/Policies/ContactMessageAccessPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\ContactMessage;
+use App\Models\User;
+
+class ContactMessageAccessPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, ContactMessage $ContactMessage): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, ContactMessage $ContactMessage): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, ContactMessage $ContactMessage): bool
+    {
+        return true;
+    }
+}
