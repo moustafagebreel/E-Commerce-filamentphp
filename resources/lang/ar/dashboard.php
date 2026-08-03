7c2330a19a3fe0bfd2fd392e6505cd384adacd37
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:46:08 +0300

    i18n(dashboard): add localized string key definitions set 7

--- resources/lang/ar/dashboard.php
+++ resources/lang/ar/dashboard.php
@@ -1,7 +1,7 @@
 ﻿<?php
 
 return [
-    'key_6' => 'قيمة عربية 6 لقسم dashboard',
-    'title_6' => 'عنوان عربي 6 لقسم dashboard',
-    'description_6' => 'وصف عربي 6 لقسم dashboard',
+    'key_7' => 'قيمة عربية 7 لقسم dashboard',
+    'title_7' => 'عنوان عربي 7 لقسم dashboard',
+    'description_7' => 'وصف عربي 7 لقسم dashboard',
 ];
--- resources/lang/en/dashboard.php
+++ resources/lang/en/dashboard.php
@@ -1,7 +1,7 @@
 ﻿<?php
 
 return [
-    'key_6' => 'Value English 6 for dashboard',
-    'title_6' => 'Title English 6 for dashboard',
-    'description_6' => 'Description English 6 for dashboard',
+    'key_7' => 'Value English 7 for dashboard',
+    'title_7' => 'Title English 7 for dashboard',
+    'description_7' => 'Description English 7 for dashboard',
 ];
