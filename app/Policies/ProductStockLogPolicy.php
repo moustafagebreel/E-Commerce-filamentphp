b32620c361049ca02c16f120462318cf61721c89
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:57:43 +0300

    feat(policies): implement ProductStockLogPolicy authorization rules

---
+++ app/Policies/ProductStockLogPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\ProductStockLog;
+use App\Models\User;
+
+class ProductStockLogPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, ProductStockLog $ProductStockLog): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, ProductStockLog $ProductStockLog): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, ProductStockLog $ProductStockLog): bool
+    {
+        return true;
+    }
+}
