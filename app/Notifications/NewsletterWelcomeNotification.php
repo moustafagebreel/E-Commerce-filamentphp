43e118ceb5623ab8335e16b0f7ddc2d006f97333
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:39:16 +0300

    feat(notifications): implement NewsletterWelcomeNotification mail class

---
+++ app/Notifications/NewsletterWelcomeNotification.php
@@ -0,0 +1,24 @@
+﻿<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NewsletterWelcomeNotification extends Notification
+{
+    use Queueable;
+
+    public function via($notifiable): array
+    {
+        return ['mail', 'database'];
+    }
+
+    public function toMail($notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('NewsletterWelcomeNotification Notice')
+            ->line('Notification update regarding your account activity.');
+    }
+}
