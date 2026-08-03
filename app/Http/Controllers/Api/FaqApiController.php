90de54d867f615dbf39e8dff9a760abd5d4ec497
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:58:41 +0300

    feat(api): add FaqApiController REST endpoint controller

---
+++ app/Http/Controllers/Api/FaqApiController.php
@@ -0,0 +1,14 @@
+﻿<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FaqApiController extends Controller
+{
+    public function index()
+    {
+        return response()->json(['message' => 'FaqApiController active']);
+    }
+}
