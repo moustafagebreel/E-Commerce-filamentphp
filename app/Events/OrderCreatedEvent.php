db762c741d0113f5c2297159e49668fa97f8fb2a
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:58:12 +0300

    feat(events): add OrderCreatedEvent system event class

---
+++ app/Events/OrderCreatedEvent.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+namespace App\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderCreatedEvent
+{
+    use Dispatchable, SerializesModels;
+
+    public function __construct()
+    {
+    }
+}
