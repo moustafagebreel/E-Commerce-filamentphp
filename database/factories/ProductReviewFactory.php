3a7b178add51fb6c5323b5b53acf31acd0cf1387
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:35:11 +0300

    test(factories): create ProductReviewFactory for unit testing and seeding

---
+++ database/factories/ProductReviewFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\ProductReview;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductReviewFactory extends Factory
+{
+    protected $model = ProductReview::class;
+
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'product_id' => Product::factory(),
+            'rating' => $this->faker->numberBetween(1, 5),
+            'headline' => $this->faker->sentence(4),
+            'comment' => $this->faker->paragraph(),
+            'is_approved' => true,
+        ];
+    }
+}
