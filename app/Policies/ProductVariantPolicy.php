8168679f827637700163a74db9c99309878b5108
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:57:41 +0300

    feat(policies): implement ProductVariantPolicy authorization rules

---
+++ app/Policies/ProductVariantPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\ProductVariant;
+use App\Models\User;
+
+class ProductVariantPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, ProductVariant $ProductVariant): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, ProductVariant $ProductVariant): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, ProductVariant $ProductVariant): bool
+    {
+        return true;
+    }
+}
