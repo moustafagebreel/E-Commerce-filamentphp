6850fb849a646f14377c172074d7d1254a5957a8
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:46:20 +0300

    i18n(errors): add localized string key definitions set 1

---
+++ resources/lang/ar/errors.php
@@ -0,0 +1,7 @@
+﻿<?php
+
+return [
+    'key_1' => 'قيمة عربية 1 لقسم errors',
+    'title_1' => 'عنوان عربي 1 لقسم errors',
+    'description_1' => 'وصف عربي 1 لقسم errors',
+];
---
+++ resources/lang/en/errors.php
@@ -0,0 +1,7 @@
+﻿<?php
+
+return [
+    'key_1' => 'Value English 1 for errors',
+    'title_1' => 'Title English 1 for errors',
+    'description_1' => 'Description English 1 for errors',
+];
