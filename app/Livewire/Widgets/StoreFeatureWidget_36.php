eb79c6043c8b806b725bc26ef6accceef5609d6d
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:42:04 +0300

    feat(storefront): implement StoreFeatureWidget_36 Livewire class and view

---
+++ app/Livewire/Widgets/StoreFeatureWidget_36.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+namespace App\Livewire\Widgets;
+
+use Livewire\Component;
+
+class StoreFeatureWidget_36 extends Component
+{
+    public function render()
+    {
+        return view('livewire.widgets.store-feature-widget-36');
+    }
+}
