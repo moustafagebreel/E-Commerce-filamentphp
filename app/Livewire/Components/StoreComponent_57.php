dffd02fac293513ed115394e3569f3434529b17f
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:01:09 +0300

    feat(livewire): implement StoreComponent_57 class and Blade template

---
+++ app/Livewire/Components/StoreComponent_57.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+namespace App\Livewire\Components;
+
+use Livewire\Component;
+
+class StoreComponent_57 extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.store-component-57');
+    }
+}
