60870d5a1034538ef575b19b733a2352f6586cc2
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:57:39 +0300

    feat(policies): implement FaqPolicy authorization rules

---
+++ app/Policies/FaqPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\Faq;
+use App\Models\User;
+
+class FaqPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, Faq $Faq): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, Faq $Faq): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, Faq $Faq): bool
+    {
+        return true;
+    }
+}
