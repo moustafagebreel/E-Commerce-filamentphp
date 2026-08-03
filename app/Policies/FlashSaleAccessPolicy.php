74209a0faffa3dc449d03d21b61de7f17efaf695
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:38:53 +0300

    feat(policies): add FlashSaleAccessPolicy authorization rules

---
+++ app/Policies/FlashSaleAccessPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\FlashSale;
+use App\Models\User;
+
+class FlashSaleAccessPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, FlashSale $FlashSale): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, FlashSale $FlashSale): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, FlashSale $FlashSale): bool
+    {
+        return true;
+    }
+}
