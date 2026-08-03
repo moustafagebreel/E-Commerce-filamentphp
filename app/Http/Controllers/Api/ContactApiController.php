2e210b66c1b934d71828fed5dbdcf05bf154be47
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:58:42 +0300

    feat(api): add ContactApiController REST endpoint controller

---
+++ app/Http/Controllers/Api/ContactApiController.php
@@ -0,0 +1,14 @@
+﻿<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ContactApiController extends Controller
+{
+    public function index()
+    {
+        return response()->json(['message' => 'ContactApiController active']);
+    }
+}
