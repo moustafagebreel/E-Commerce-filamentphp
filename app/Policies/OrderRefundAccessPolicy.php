93f0324a97b967b31a4e2908565eb32c54b92026
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:38:48 +0300

    feat(policies): add OrderRefundAccessPolicy authorization rules

---
+++ app/Policies/OrderRefundAccessPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\OrderRefund;
+use App\Models\User;
+
+class OrderRefundAccessPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, OrderRefund $OrderRefund): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, OrderRefund $OrderRefund): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, OrderRefund $OrderRefund): bool
+    {
+        return true;
+    }
+}
