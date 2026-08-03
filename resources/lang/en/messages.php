0a50c4c097082f8bc92fa63c04205a406cd5a957
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:32:19 +0300

    i18n(en): add general messages translation dictionary

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'welcome' => 'Welcome to our store',
+    'search_placeholder' => 'Search products, categories, brands...',
+    'all_rights_reserved' => 'All rights reserved.',
+    'contact_us' => 'Contact Us',
+    'faq' => 'Frequently Asked Questions',
+    'home' => 'Home',
+    'categories' => 'Categories',
+    'products' => 'Products',
+    'cart' => 'Cart',
+    'wishlist' => 'Wishlist',
+    'my_orders' => 'My Orders',
+    'addresses' => 'Saved Addresses',
+    'profile' => 'Profile',
+    'login' => 'Sign In',
+    'register' => 'Register',
+    'logout' => 'Log Out',
+];
