d24fcbaf4acaeeeebb8766f61090e16b24fcf4fd
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:57:32 +0300

    feat(policies): implement UserPolicy authorization rules

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\User;
+
+class UserPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, User $User): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, User $User): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, User $User): bool
+    {
+        return true;
+    }
+}
