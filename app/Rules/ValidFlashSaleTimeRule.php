8900cacd8ec5f15798cf56913eb266aa35a8219f
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:39:03 +0300

    feat(rules): implement custom validation rule ValidFlashSaleTimeRule

---
+++ app/Rules/ValidFlashSaleTimeRule.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class ValidFlashSaleTimeRule implements ValidationRule
+{
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (empty($value)) {
+            $fail("The :attribute validation failed.");
+        }
+    }
+}
