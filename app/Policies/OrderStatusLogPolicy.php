14c1af6e027ce4100badd3af0cbd57b7a3cc9eb4
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:57:42 +0300

    feat(policies): implement OrderStatusLogPolicy authorization rules

---
+++ app/Policies/OrderStatusLogPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\OrderStatusLog;
+use App\Models\User;
+
+class OrderStatusLogPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, OrderStatusLog $OrderStatusLog): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, OrderStatusLog $OrderStatusLog): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, OrderStatusLog $OrderStatusLog): bool
+    {
+        return true;
+    }
+}
