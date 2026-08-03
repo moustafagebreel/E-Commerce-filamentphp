1ba22583489249790be546a8d8d8960556dca28a
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:38:54 +0300

    feat(policies): add NewsletterSubscriberAccessPolicy authorization rules

---
+++ app/Policies/NewsletterSubscriberAccessPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\NewsletterSubscriber;
+use App\Models\User;
+
+class NewsletterSubscriberAccessPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, NewsletterSubscriber $NewsletterSubscriber): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, NewsletterSubscriber $NewsletterSubscriber): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, NewsletterSubscriber $NewsletterSubscriber): bool
+    {
+        return true;
+    }
+}
