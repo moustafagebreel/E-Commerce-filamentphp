dda6e2de7091a0361d2c0951546cb225bb4d096b
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:57:37 +0300

    feat(policies): implement ProductReviewPolicy authorization rules

---
+++ app/Policies/ProductReviewPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\ProductReview;
+use App\Models\User;
+
+class ProductReviewPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, ProductReview $ProductReview): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, ProductReview $ProductReview): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, ProductReview $ProductReview): bool
+    {
+        return true;
+    }
+}
