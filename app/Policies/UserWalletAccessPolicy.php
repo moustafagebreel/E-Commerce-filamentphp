77d39dcfdc027c4af7fdde0a89fad3fdd13ed783
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:38:52 +0300

    feat(policies): add UserWalletAccessPolicy authorization rules

---
+++ app/Policies/UserWalletAccessPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\UserWallet;
+use App\Models\User;
+
+class UserWalletAccessPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, UserWallet $UserWallet): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, UserWallet $UserWallet): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, UserWallet $UserWallet): bool
+    {
+        return true;
+    }
+}
