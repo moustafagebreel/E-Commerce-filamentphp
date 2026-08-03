b09fa7335e39d8966a160da3e8d63952738dd3d4
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:57:42 +0300

    feat(policies): implement SiteSettingPolicy authorization rules

---
+++ app/Policies/SiteSettingPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\SiteSetting;
+use App\Models\User;
+
+class SiteSettingPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, SiteSetting $SiteSetting): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, SiteSetting $SiteSetting): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, SiteSetting $SiteSetting): bool
+    {
+        return true;
+    }
+}
