e891adb7520cbcd9e88d55fcbaad567954cfea30
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:45:46 +0300

    i18n(checkout): add localized string key definitions set 6

--- resources/lang/ar/checkout.php
+++ resources/lang/ar/checkout.php
@@ -1,7 +1,7 @@
 ﻿<?php
 
 return [
-    'key_5' => 'قيمة عربية 5 لقسم checkout',
-    'title_5' => 'عنوان عربي 5 لقسم checkout',
-    'description_5' => 'وصف عربي 5 لقسم checkout',
+    'key_6' => 'قيمة عربية 6 لقسم checkout',
+    'title_6' => 'عنوان عربي 6 لقسم checkout',
+    'description_6' => 'وصف عربي 6 لقسم checkout',
 ];
