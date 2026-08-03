689a44617dcbf28f02d6ef604fc3e936964cd865
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:38:51 +0300

    feat(policies): add ShippingZoneAccessPolicy authorization rules

---
+++ app/Policies/ShippingZoneAccessPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\ShippingZone;
+use App\Models\User;
+
+class ShippingZoneAccessPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, ShippingZone $ShippingZone): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, ShippingZone $ShippingZone): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, ShippingZone $ShippingZone): bool
+    {
+        return true;
+    }
+}
