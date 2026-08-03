af7c412c335ef744ee5a3e0edc405dd901f1fe3c
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:38:46 +0300

    feat(policies): add AddressAccessPolicy authorization rules

---
+++ app/Policies/AddressAccessPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\Address;
+use App\Models\User;
+
+class AddressAccessPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, Address $Address): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, Address $Address): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, Address $Address): bool
+    {
+        return true;
+    }
+}
