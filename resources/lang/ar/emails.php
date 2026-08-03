a3f1f5b2d42c27d8cdc9930ac98c5f41e3caff13
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:46:44 +0300

    i18n(emails): add localized string key definitions set 1

---
+++ resources/lang/ar/emails.php
@@ -0,0 +1,7 @@
+﻿<?php
+
+return [
+    'key_1' => 'قيمة عربية 1 لقسم emails',
+    'title_1' => 'عنوان عربي 1 لقسم emails',
+    'description_1' => 'وصف عربي 1 لقسم emails',
+];
---
+++ resources/lang/en/emails.php
@@ -0,0 +1,7 @@
+﻿<?php
+
+return [
+    'key_1' => 'Value English 1 for emails',
+    'title_1' => 'Title English 1 for emails',
+    'description_1' => 'Description English 1 for emails',
+];
