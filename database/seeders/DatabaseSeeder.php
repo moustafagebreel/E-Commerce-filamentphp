d15c331e39b9f5dc800dcef4d8b161685df9c266
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:31:51 +0300

    feat(seeders): register all newly created seeders in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-            // \App\Models\User::factory(10)->create();
-
-            // \App\Models\User::factory()->create([
-            //     'name' => 'Test User',
-            //     'email' => 'test@example.com',
-            // ]);
+        $this->call([
+            SiteSettingSeeder::class,
+            BannerSeeder::class,
+            CouponSeeder::class,
+            FaqSeeder::class,
+            ProductReviewSeeder::class,
+        ]);
     }
 }
+
