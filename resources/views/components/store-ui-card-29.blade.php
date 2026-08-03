a92e9ba321d938dd4d224efc4b115c7cfe459b4b
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:40:45 +0300

    feat(ui): add <x-store-ui-card-29> blade component template

---
+++ resources/views/components/store-ui-card-29.blade.php
@@ -0,0 +1,6 @@
+﻿@props(['title' => 'UI Component #29'])
+
+<div class="p-4 bg-white dark:bg-slate-900 rounded-xl border border-slate-200 dark:border-slate-800 shadow-sm">
+    <h5 class="font-bold text-slate-800 dark:text-slate-200 text-sm">{{ $title }}</h5>
+    {{ $slot }}
+</div>
