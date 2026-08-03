ab5398d23ab72782f397d4ac6e5a95480964153f
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:38:39 +0300

    feat(policies): add BrandAccessPolicy authorization rules

---
+++ app/Policies/BrandAccessPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\Brand;
+use App\Models\User;
+
+class BrandAccessPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, Brand $Brand): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, Brand $Brand): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, Brand $Brand): bool
+    {
+        return true;
+    }
+}
