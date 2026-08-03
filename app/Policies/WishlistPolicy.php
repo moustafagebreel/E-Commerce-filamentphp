5a3842ef5cb26a8c59bc628c7f8d4f5e5d4bed52
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:57:38 +0300

    feat(policies): implement WishlistPolicy authorization rules

---
+++ app/Policies/WishlistPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\Wishlist;
+use App\Models\User;
+
+class WishlistPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, Wishlist $Wishlist): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, Wishlist $Wishlist): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, Wishlist $Wishlist): bool
+    {
+        return true;
+    }
+}
