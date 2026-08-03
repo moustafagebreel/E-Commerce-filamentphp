f5322e767713c6efbbb6aee4fcf803735d021b97
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:45:14 +0300

    i18n(auth): add localized string key definitions set 17

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -1,7 +1,7 @@
 ﻿<?php
 
 return [
-    'key_16' => 'Value English 16 for auth',
-    'title_16' => 'Title English 16 for auth',
-    'description_16' => 'Description English 16 for auth',
+    'key_17' => 'Value English 17 for auth',
+    'title_17' => 'Title English 17 for auth',
+    'description_17' => 'Description English 17 for auth',
 ];
