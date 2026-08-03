d51f5c5b037786f220726cab89f7fe069dccd2b4
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:45:42 +0300

    i18n(checkout): add localized string key definitions set 1

--- resources/lang/ar/checkout.php
+++ resources/lang/ar/checkout.php
@@ -1,16 +1,7 @@
-<?php
+﻿<?php
 
 return [
-    'checkout_title' => 'إتمام الطلب',
-    'shipping_address' => 'عنوان الشحن',
-    'payment_method' => 'طريقة الدفع',
-    'order_summary' => 'ملخص الطلب',
-    'subtotal' => 'المجموع الفرعي',
-    'shipping_fee' => 'رسوم الشحن',
-    'coupon_discount' => 'خصم القسيمة',
-    'grand_total' => 'المجموع الكلي',
-    'apply_coupon' => 'تطبيق القسيمة',
-    'place_order' => 'تأكيد الطلب الآن',
-    'cash_on_delivery' => 'الدفع عند الاستلام',
-    'stripe' => 'بطاقة ائتمان / Stripe',
+    'key_1' => 'قيمة عربية 1 لقسم checkout',
+    'title_1' => 'عنوان عربي 1 لقسم checkout',
+    'description_1' => 'وصف عربي 1 لقسم checkout',
 ];
--- resources/lang/en/checkout.php
+++ resources/lang/en/checkout.php
@@ -1,16 +1,7 @@
-<?php
+﻿<?php
 
 return [
-    'checkout_title' => 'Checkout Overview',
-    'shipping_address' => 'Shipping Address',
-    'payment_method' => 'Payment Method',
-    'order_summary' => 'Order Summary',
-    'subtotal' => 'Subtotal',
-    'shipping_fee' => 'Shipping Fee',
-    'coupon_discount' => 'Coupon Discount',
-    'grand_total' => 'Grand Total',
-    'apply_coupon' => 'Apply Coupon',
-    'place_order' => 'Place Order',
-    'cash_on_delivery' => 'Cash On Delivery',
-    'stripe' => 'Stripe / Credit Card',
+    'key_1' => 'Value English 1 for checkout',
+    'title_1' => 'Title English 1 for checkout',
+    'description_1' => 'Description English 1 for checkout',
 ];
