30082f496972a2ebf3a129cc4502f4ebef269970
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:38:39 +0300

    feat(policies): add OrderAccessPolicy authorization rules

---
+++ app/Policies/OrderAccessPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\Order;
+use App\Models\User;
+
+class OrderAccessPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, Order $Order): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, Order $Order): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, Order $Order): bool
+    {
+        return true;
+    }
+}
