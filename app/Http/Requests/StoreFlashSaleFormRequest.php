3d0dd726acef5674614c5fc6f061624ed19be806
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:38:29 +0300

    feat(requests): implement StoreFlashSaleFormRequest validation class

---
+++ app/Http/Requests/StoreFlashSaleFormRequest.php
@@ -0,0 +1,20 @@
+﻿<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreFlashSaleFormRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            // Custom rules for StoreFlashSaleFormRequest
+        ];
+    }
+}
