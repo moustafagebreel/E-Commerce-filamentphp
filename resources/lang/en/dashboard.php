503cc9e1a640bc0909254deae60e2bb96df78a49
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:46:17 +0300

    i18n(dashboard): add localized string key definitions set 19

--- resources/lang/en/dashboard.php
+++ resources/lang/en/dashboard.php
@@ -1,7 +1,7 @@
 ﻿<?php
 
 return [
-    'key_18' => 'Value English 18 for dashboard',
-    'title_18' => 'Title English 18 for dashboard',
-    'description_18' => 'Description English 18 for dashboard',
+    'key_19' => 'Value English 19 for dashboard',
+    'title_19' => 'Title English 19 for dashboard',
+    'description_19' => 'Description English 19 for dashboard',
 ];
