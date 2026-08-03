8cefa92b1d391ee7f73bdeb4d7fac0c804419488
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:35:32 +0300

    feat(models): add ProductAttributeValue model

---
+++ app/Models/ProductAttributeValue.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductAttributeValue extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_attribute_id',
+        'value',
+        'color_code',
+    ];
+
+    public function attribute()
+    {
+        return $this->belongsTo(ProductAttribute::class, 'product_attribute_id');
+    }
+}
