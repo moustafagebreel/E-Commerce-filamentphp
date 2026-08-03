4f0401eafc84a3b34d10d61d9f83d46a7fed0ee3
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:01:22 +0300

    feat(livewire): implement StoreComponent_65 class and Blade template

---
+++ resources/views/livewire/components/store-component-65.blade.php
@@ -0,0 +1,3 @@
+﻿<div class="p-4 bg-white dark:bg-slate-900 rounded-lg border border-slate-200 dark:border-slate-800">
+    <h4 class="font-bold text-slate-800 dark:text-slate-200">Store Component #65</h4>
+</div>
