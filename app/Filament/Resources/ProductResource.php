9917350e439e8d54baa6ed4cd957be7b0de35908
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:08:15 +0300

    feat: implement e-commerce application structure with Livewire components and Filament resources

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Product;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Illuminate\Support\Str;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Forms\Components\FileUpload;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Forms\Components\MarkdownEditor;
+use App\Filament\Resources\ProductResource\Pages;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+    public static function getNavigationBadge(): ?string
+{
+    return static::getModel()::count();
+}
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+
+                Group::make()->schema([
+                    Section::make('product information')->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->live(onBlur: true)
+                            ->afterStateUpdated(fn($set, $get) => $set('slug', Str::slug($get('name'))))
+                            ->maxLength(255),
+
+                        TextInput::make('slug')
+                            ->required()
+                            ->maxLength(255)
+                            ->unique(Product::class, 'slug', ignoreRecord: true)
+                            ->dehydrated(),
+
+                        MarkdownEditor::make('description')
+                            ->columnSpanFull()
+                            ->fileAttachmentsDirectory('products'),
+
+                    ])->columns(2),
+
+                    Section::make('images')->schema([
+                        FileUpload::make('images')
+                            ->multiple()
+                            ->directory('products')
+                            ->maxFiles(5)
+                            ->columnSpanFull(),
+                    ]),
+                ])->columnSpan(2),
+
+                Group::make()->schema([
+
+                    Section::make('price')
+                        ->schema([
+                            TextInput::make('price')
+                                ->required()
+                                ->prefix('$')
+                                ->type('number')
+                                ->step('0.01'),
+
+                        ])->columnSpan(1),
+
+                    Section::make('status')->schema([
+                        Toggle::make('is_active')
+                            ->default(false),
+                        Toggle::make('is_featured')
+
+                            ->default(false),
+                        Toggle::make('on_sale')
+                            ->default(false),
+                        Toggle::make('in_stock')
+                            ->default(false),
+                    ])->columns(1),
+
+                    Section::make('association')->schema([
+
+                        Select::make('category_id')
+                            ->required()
+                            ->relationship('category', 'name')
+                            ->preload(),
+
+                        Select::make('brand_id')
+                            ->required()
+                            ->relationship('brand', 'name')
+                            ->preload(),
+                    ]),
+
+                ]),
+
+            ])->columns(3);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('category.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('brand.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('slug')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('price')
+                    ->money()
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('is_active')
+                    ->boolean(),
+                Tables\Columns\IconColumn::make('is_featured')
+                    ->boolean(),
+                Tables\Columns\IconColumn::make('on_sale')
+                    ->boolean(),
+                Tables\Columns\IconColumn::make('in_stock')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\ImageColumn::make('images'),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //  
+                SelectFilter::make('category_id')
+                    ->relationship('category', 'name'),
+                    SelectFilter::make('brand_id')
+                    ->relationship('brand', 'name')
+            ])
+
+                    
+            
+            ->actions([
+                ActionGroup::make([
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ]),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+}
