9027d1adc0e79e0ebb33b6c1d8e20a9e817a34e2
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:33:55 +0300

    i18n(en): add store catalog and product listing translations

---
+++ resources/lang/en/store.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'filter' => 'Filter Products',
+    'categories' => 'Categories',
+    'brands' => 'Brands',
+    'price_range' => 'Price Range',
+    'sort_by' => 'Sort By',
+    'featured' => 'Featured Products',
+    'on_sale' => 'On Sale',
+    'in_stock' => 'In Stock Only',
+    'add_to_cart' => 'Add to Cart',
+    'view_details' => 'View Details',
+    'out_of_stock' => 'Out of Stock',
+    'customer_reviews' => 'Customer Reviews',
+    'write_review' => 'Write a Review',
+];
