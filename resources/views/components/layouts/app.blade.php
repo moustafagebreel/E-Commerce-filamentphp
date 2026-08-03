2f0d99fbd904b327c06534c90d93ebe7319349ff
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:15:10 +0300

    feat: implement frontend views for e-commerce storefront including authentication, products, cart, checkout, and order management pages

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+
+        <title>{{ $title ?? 'MOGEBREEL' }}</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @livewireStyles
+    </head>
+    <body  id="body" class="bg-slate-200  dark:bg-slate-700"  >
+        @livewire('partials.navbar')
+  {{-- <main class="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto"> --}}
+
+        <main>
+            {{ $slot }}
+        </main>
+
+
+        @livewire('partials.footer')
+
+
+        @livewireScripts
+
+
+        @vite(['resources/js/dark-mode.js'])
+    </body> 
+</html>
