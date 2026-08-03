4661e38414a92bf915535a1716fab10802e208bf
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:38:53 +0300

    feat(policies): add ProductAttributeAccessPolicy authorization rules

---
+++ app/Policies/ProductAttributeAccessPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\ProductAttribute;
+use App\Models\User;
+
+class ProductAttributeAccessPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, ProductAttribute $ProductAttribute): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, ProductAttribute $ProductAttribute): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, ProductAttribute $ProductAttribute): bool
+    {
+        return true;
+    }
+}
