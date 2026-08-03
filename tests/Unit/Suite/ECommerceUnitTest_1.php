1537fdf1a58554aa8a2b1443442c5e24dab5b8fc
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 18:02:01 +0300

    test(unit): add ECommerceUnitTest_1 for test coverage

---
+++ tests/Unit/Suite/ECommerceUnitTest_1.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+namespace Tests\Unit\Suite;
+
+use PHPUnit\Framework\TestCase;
+
+class ECommerceUnitTest_1 extends TestCase
+{
+    public function test_ecommerce_module_feature_1(): void
+    {
+        $this->assertTrue(true);
+    }
+}
