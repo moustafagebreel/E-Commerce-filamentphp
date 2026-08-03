d2d13bc147537ff6ea61ac4da4fcd0a28340fde4
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:57:33 +0300

    feat(policies): implement CategoryPolicy authorization rules

---
+++ app/Policies/CategoryPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\Category;
+use App\Models\User;
+
+class CategoryPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, Category $Category): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, Category $Category): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, Category $Category): bool
+    {
+        return true;
+    }
+}
