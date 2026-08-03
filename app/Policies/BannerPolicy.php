9e6e751b68ad66ebcb12931bb75d178010033f9e
moustafagebreel <moustafagebreeel@gmail.com>
2026-08-03 17:57:36 +0300

    feat(policies): implement BannerPolicy authorization rules

---
+++ app/Policies/BannerPolicy.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace App\Policies;
+
+use App\Models\Banner;
+use App\Models\User;
+
+class BannerPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, Banner $Banner): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, Banner $Banner): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, Banner $Banner): bool
+    {
+        return true;
+    }
+}
